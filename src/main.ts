abdd03209313ad52d84ca4b4b4d3fe29de22d5f9
sesychev <sergeysychou@gmail.com>
2023-02-02 16:05:27 +0300

    feat: add config

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+if (!process.env.PORT) {
+  process.exit(1);
+}
+
+const PORT = process.env.PORT || 4000;
+const HOST = process.env.HOST;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(4000);
+  await app.listen(PORT, () =>
+    console.log(
+      `Server running on ${PORT} port as a http://${HOST}:${PORT} service`,
+    ),
+  );
 }
+
 bootstrap();
