69529f47631d335fb65a8d14a88d3017b6db8cb8
sesychev <sergeysychou@gmail.com>
2023-02-04 19:36:57 +0300

    feat: tracks done

---
+++ src/tracks/tracks.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@nestjs/common';
+import { TracksModel } from '../tracks/model/tracks.model';
+import { validate } from 'uuid';
+import { BadRequestException } from 'src/common/common.errors';
+import { CreateTrackDto, UpdateTrackDto } from './dto/create-track.dto';
+
+@Injectable()
+export class TracksService {
+  constructor(private readonly tracksModel: TracksModel) {}
+
+  async findAll() {
+    return await this.tracksModel.findAll();
+  }
+
+  async findOne(id: string) {
+    if (validate(id)) return await this.tracksModel.findOne(id);
+    else throw new BadRequestException();
+  }
+
+  async post(dto: CreateTrackDto) {
+    return await this.tracksModel.post(dto);
+  }
+
+  async put(id: string, dto: UpdateTrackDto) {
+    if (!validate(id)) throw new BadRequestException();
+    if (typeof dto.name !== 'string') throw new BadRequestException();
+    return this.tracksModel.put(id, dto);
+  }
+
+  async delete(id: string) {
+    if (validate(id)) return await this.tracksModel.delete(id);
+    else throw new BadRequestException();
+  }
+}
